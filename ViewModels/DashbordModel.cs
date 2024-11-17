04afafc136c3a162ea0f62280b8871ba4bf18b5d
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2024-11-17 14:09:00 +0100

    Add Png and make dashbord template

--- ViewModels/DashbordModel.cs
+++ ViewModels/DashbordModel.cs
@@ -8,9 +8,31 @@ namespace WPF_Pocket_Trainer.ViewModels
 {
     class DashbordModel : ObservableObject
     {
+        public RelayCommand DashboardViewCommand { get; set; }
+        public RelayCommand SettingsViewCommand { get; set; }
+
+        public TrainignsViewModel TrainignsVM { get; set; }
+        public SettingViewModel SettingVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set {
+                _currentView = value;
+                OnPropertyChange();
+            }
+            
+        }
+
+
+
         public DashbordModel()
         {
-             
+            TrainignsVM = new TrainignsViewModel();
+            SettingVM = new SettingViewModel();
+            CurrentView = TrainignsVM;
         }
     }
 }
