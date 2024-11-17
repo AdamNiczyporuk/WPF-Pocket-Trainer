e7bb4cde267e5a27e5d27cd02b41d08c93673ebd
Adam Niczyporuk <60665323+AmoxV2@users.noreply.github.com>
2024-11-17 13:26:11 +0100

    Add New View trainings

---
+++ ViewModels/DashbordModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_Pocket_Trainer.Dashboard.Core;
+namespace WPF_Pocket_Trainer.ViewModels
+{
+    class DashbordModel : ObservableObject
+    {
+        public DashbordModel()
+        {
+             
+        }
+    }
+}
